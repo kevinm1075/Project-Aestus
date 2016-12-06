71389f99ed991d77fbdbf53c4a70e7a07310a545
kevin <kevin@DESKTOP-GGI3LBM>
2016-12-06 17:56:14 -0500

    Initial commit

---
+++ Project Aestus/Project Aestus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Aestus.Startup))]
+namespace Project_Aestus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
